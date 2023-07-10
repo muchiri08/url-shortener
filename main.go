1b31581b190cbe72c4a9553783d52b1d04d40098
muchiri08 <mbogokennedy08@gmail.com>
2023-07-10 22:54:20 +0300

    add generate short url

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	base62 "github.com/muchiri08/urlshortener/utils"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -18,6 +19,7 @@ type Record struct {
 	URL string `json:"url"`
 }
 
+// GetOriginalURL fetches the original URL for the given encoded(short)
 func (driver *DBClient) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
 	var url string
 	vars := mux.Vars(r)
@@ -32,6 +34,23 @@ func (driver *DBClient) GetOriginalURL(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Set("Content-Type", "application/json")
 		responseMap := map[string]interface{}{"url": url}
+		response, _ := json.Marshal(responseMap)
+		w.Write(response)
+	}
+}
+
+// GenerateShortURL adds URL to DB and gives back shortened string
+func (driver *DBClient) GenerateShortURL(w http.ResponseWriter, r *http.Request) {
+	var id int
+	var record Record
+	postBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(postBody, &record)
+	err := driver.db.QueryRow("INSERT INTO web_url(url) VALUES($1) RETURNING id", record.URL).Scan(&id)
+	responseMap := map[string]interface{}{"encoded_string": base62.ToBase62(id)}
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	} else {
+		w.Header().Set("Content-Type", "application/json")
 		response, _ := json.Marshal(responseMap)
 		w.Write(response)
 	}
