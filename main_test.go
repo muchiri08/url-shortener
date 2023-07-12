43eb5c033d2ce74d5a179b78c424b626b1f5a17b
muchiri08 <mbogokennedy08@gmail.com>
2023-07-12 21:31:00 +0300

    add main_test file

---
+++ main_test.go
@@ -0,0 +1,18 @@
+package main_test
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetOriginalURL(t *testing.T) {
+	//Make a dummy request
+	response, err := http.Get("http://localhost:8000/v1/short/1")
+	if http.StatusOK != response.StatusCode {
+		t.Errorf("Expected response code %d. Got %d\n", http.StatusOK, response.StatusCode)
+	}
+
+	if err != nil {
+		t.Error("Encountered an error: ", err)
+	}
+}
